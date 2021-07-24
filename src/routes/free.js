ebb79089d5335df2f840a70575ef119a8d2a6481
Ali MOUSAZADEH <ali.moosazadeh92@gmail.com>
2021-07-24 21:28:22 +0430

    audio added

--- src/routes/free.js
+++ src/routes/free.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import { withRouter, useHistory } from "react-router-dom";
+// import free_audio from "../assets/audio/free.mp3";
 
 const Free = () => {
   return (
-    <div className="min-vh-100 bg-success min-vw-100 d-flex align-items-center justify-content-center">
-      <span className="display-1 text-white fw-bolder">شما بردید!</span>
+    <div className="min-vh-100 bg-success min-vw-100 flex-column d-flex align-items-center justify-content-center">
+      <div className="col-12 mb-5 text-center">
+        <span className="display-1 text-white fw-bolder">شما بردید!</span>
+      </div>
+      <div className="col-12 text-center">
+        {/* <audio controls autoplay>
+          <source src={free_audio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio> */}
+      </div>
     </div>
   );
 };
--- src/routes/lost.js
+++ src/routes/lost.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import { withRouter, useHistory } from "react-router-dom";
+import lost_audio from "../assets/audio/lost.mp3";
 
 const Lost = () => {
   return (
-    <div className="min-vh-100 bg-danger min-vw-100 d-flex align-items-center justify-content-center">
-      <span className="display-1 text-white fw-bolder">شما باختید!</span>
+    <div className="min-vh-100 bg-danger min-vw-100 flex-column d-flex align-items-center justify-content-center">
+      <div className="col-12 mb-5 text-center">
+        <span className="display-1 text-white fw-bolder">شما باختید!</span>
+      </div>
+      <div className="col-12 text-center">
+        <audio controls autoplay>
+          <source src={lost_audio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
     </div>
   );
 };
